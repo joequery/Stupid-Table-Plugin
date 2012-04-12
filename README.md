5447ea437c5a86c6ade42df9d858eb155f4a29a1
Joseph McCullough <joseph@vertstudios.com>
2012-04-12 13:47:59 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Stupid jQuery Table Sort
+========================
+
+This is a stupid jQuery table sorting plugin. Nothing fancy, nothing really
+impressive. Overall, stupidly simple.
+
+See the example.html document to see how to implement it. All you really
+need to do differently is make sure you're using ```<thead>``` and 
+```<tbody>``` in case you don't normally use those elements.
---
+++ stupidtable.js
@@ -0,0 +1,99 @@
+// Stupid jQuery table plugin.
+
+// Call on a table 
+(function($){
+  $.fn.stupidtable = function(x){
+    var table = this;
+
+    // ==================================================== //
+    //                  Utility functions                   //
+    // ==================================================== //
+    // Return the resulting indexes of a sort so we can apply
+    // this result elsewhere. This returns an array of index numbers.
+    // return[0] = x means "arr's 0th element is now at x"
+    var sort_map =  function(arr){
+      sorted = arr.slice(0).sort(); // slice to create clone
+      map = [];
+      for(var i=0; i<arr.length; i++){
+        map.push(sorted.indexOf(arr[i]));
+      }
+      return map;
+    }
+
+    // Apply a sort map to the array. Alters arr
+    var apply_sort_map = function(arr, map){
+      clone = arr.slice(0);
+      for(var i=0; i<map.length; i++){
+        newIndex = map[i];
+        clone[newIndex] = arr[i];
+      }
+
+      return clone;
+    }
+
+    // Returns true if array is sorted, false otherwise.
+    // Checks for both ascending and descending
+    var is_sorted_array = function(arr){
+      var isAscending = true;
+      var isDescending = true;
+      for(var i=0; i<arr.length-1; i++){
+        if(arr[i+1] > arr[i]){
+          isAscending = false;
+          break;
+        }
+      }
+
+      for(var i=arr.length-1; i>0; i--){
+        if(arr[i-1] > arr[i]){
+          isDescending = false;
+          break;
+        }
+      }
+      return (isAscending || isDescending);
+    }
+
+    // ==================================================== //
+    //                  Begin execution!                    //
+    // ==================================================== //
+    // Do sorting when THs are clicked
+    table.delegate("th", "click", function(){
+      trs = table.find("tr").slice(1); // Don't include headers
+      i = $(this).index();
+
+      // Gather the elements for this column
+      column = [];
+
+      // Push the text in this column to column[] for string comparison.
+      trs.each(function(index,tr){
+        e = $(tr).children().eq(i);
+        column.push(e.text());
+      });
+
+      // If the column is already sorted, just reverse the order. The sort
+      // map is just reversing the indexes.
+      if(is_sorted_array(column)){
+        column.reverse();
+        theMap = [];
+        for(var i=column.length-1; i>=0; i--){
+          theMap.push(i);
+        }
+      }
+      else{
+        // Get a sort map and apply to all rows
+        theMap = sort_map(column);
+      }
+
+      sortedTRs = apply_sort_map(trs, theMap);
+      // Get all the rows as their native html strings
+      newHTML = "";
+      $(sortedTRs).each(function(index, e){
+        // Hackish, we need the outerHTML to preserve TR styles.
+        // See here for more details: http://stackoverflow.com/a/4741203
+        newHTML += $(e).clone().wrap('<div>').parent().html();
+      });
+      
+      // Replace the table body html with the new html
+      table.find("tbody").html(newHTML);
+    });
+  }
+ })(jQuery);
