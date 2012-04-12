c11ed7f91a742da2a03fa1e17ce8edabcd4cbe3e
Joseph McCullough <joseph@vertstudios.com>
2012-04-12 14:57:10 -0500

    Added vars. Don't wanna polute global

--- stupidtable.js
+++ stupidtable.js
@@ -14,6 +14,7 @@
     var sort_map =  function(arr){
       var sorted = arr.slice(0).sort(); // slice to create clone
       var map = [];
+      var index = 0;
       for(var i=0; i<arr.length; i++){
         index = sorted.indexOf(arr[i]);
 
@@ -30,7 +31,7 @@
 
     // Apply a sort map to the array. Alters arr
     var apply_sort_map = function(arr, map){
-      clone = arr.slice(0);
+      var clone = arr.slice(0);
       for(var i=0; i<map.length; i++){
         newIndex = map[i];
         clone[newIndex] = arr[i];
@@ -65,15 +66,15 @@
     // ==================================================== //
     // Do sorting when THs are clicked
     table.delegate("th", "click", function(){
-      trs = table.find("tr").slice(1); // Don't include headers
-      i = $(this).index();
+      var trs = table.find("tr").slice(1); // Don't include headers
+      var i = $(this).index();
 
       // Gather the elements for this column
       column = [];
 
       // Push the text in this column to column[] for string comparison.
       trs.each(function(index,tr){
-        e = $(tr).children().eq(i);
+        var e = $(tr).children().eq(i);
         column.push(e.text());
       });
 
@@ -81,7 +82,7 @@
       // map is just reversing the indexes.
       if(is_sorted_array(column)){
         column.reverse();
-        theMap = [];
+        var theMap = [];
         for(var i=column.length-1; i>=0; i--){
           theMap.push(i);
         }
@@ -91,9 +92,9 @@
         theMap = sort_map(column);
       }
 
-      sortedTRs = apply_sort_map(trs, theMap);
+      var sortedTRs = apply_sort_map(trs, theMap);
       // Get all the rows as their native html strings
-      newHTML = "";
+      var newHTML = "";
       $(sortedTRs).each(function(index, e){
         // Hackish, we need the outerHTML to preserve TR styles.
         // See here for more details: http://stackoverflow.com/a/4741203
