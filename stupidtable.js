09d2efb2c328150fca66f11fc68af34f2e45f022
Joseph McCullough <joseph@vertstudios.com>
2012-04-30 07:55:06 -0500

    Implement data-sort-by. Closes #14

--- stupidtable.js
+++ stupidtable.js
@@ -94,12 +94,11 @@
       // Gather the elements for this column
       column = [];
 
-      // Push the text in this column to column[] for comparison.
+      // Push either the value of the 'data-order-by' attribute if specified
+      // or just the text() value in this column to column[] for comparison.
       trs.each(function(index,tr){
         var e = $(tr).children().eq(i);
-        var order_by = $(e).attr('data-order-by');
-        if (order_by == undefined)
-            order_by = e.text();
+        var order_by = e.attr('data-order-by') || e.text();
 	      column.push(order_by);
       });
 
