14325a7fa308cea8043ea1aec518a2521d4b4a80
Scott Vivian <scott.vivian@gmail.com>
2013-03-24 20:56:46 +0000

    Add case-insensitive sorting option, `string-ins`

--- stupidtable.js
+++ stupidtable.js
@@ -25,6 +25,13 @@
           if (a < b) return -1;
           if (a > b) return +1;
           return 0;
+        },
+        "string-ins": function(a, b) {
+          a = a.toLowerCase();
+          b = b.toLowerCase();
+          if (a < b) return -1;
+          if (a > b) return +1;
+          return 0;
         }
       }, sortFns);
 
