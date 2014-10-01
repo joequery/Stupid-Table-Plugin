619852b3d225051b751e5bc844ba192bb6ff0aa1
gadni <gadni@yahoo.com>
2014-10-01 13:07:02 +0300

    Core sorting logic moved in separate method so that it could be called directly from user code, method for removal of sortclicking

--- stupidtable.js
+++ stupidtable.js
@@ -11,7 +11,9 @@
 
       // Merge sort functions with some default sort functions.
       sortFns = $.extend({}, $.fn.stupidtable.default_sort_fns, sortFns);
-
+	  
+      // Preserve the sort functions
+      $table.data("sortFns", sortFns);
 
       // ==================================================== //
       //                  Begin execution!                    //
@@ -34,60 +36,104 @@
         if ($this.data("sort-dir"))
            sort_dir = $this.data("sort-dir") === dir.ASC ? dir.DESC : dir.ASC;
 
-        // Choose appropriate sorting function.
-        var type = $this.data("sort") || null;
-
-        // Prevent sorting if no type defined
-        if (type === null) {
-          return;
-        }
-
-        // Trigger `beforetablesort` event that calling scripts can hook into;
-        // pass parameters for sorted column index and sorting direction
-        $table.trigger("beforetablesort", {column: th_index, direction: sort_dir});
-        // More reliable method of forcing a redraw
-        $table.css("display");
-
-        // Run sorting asynchronously on a timout to force browser redraw after
-        // `beforetablesort` callback. Also avoids locking up the browser too much.
-        setTimeout(function() {
-          // Gather the elements for this column
-          var column = [];
-          var sortMethod = sortFns[type];
-          var trs = $table.children("tbody").children("tr");
-
-          // Extract the data for the column that needs to be sorted and pair it up
-          // with the TR itself into a tuple
-          trs.each(function(index,tr) {
-            var $e = $(tr).children().eq(th_index);
-            var sort_val = $e.data("sort-value");
-            var order_by = typeof(sort_val) !== "undefined" ? sort_val : $e.text();
-            column.push([order_by, tr]);
-          });
-
-          // Sort by the data-order-by value
-          column.sort(function(a, b) { return sortMethod(a[0], b[0]); });
-          if (sort_dir != dir.ASC)
-            column.reverse();
-
-          // Replace the content of tbody with the sorted rows. Strangely (and
-          // conveniently!) enough, .append accomplishes this for us.
-          trs = $.map(column, function(kv) { return kv[1]; });
-          $table.children("tbody").append(trs);
-
-          // Reset siblings
-          $table.find("th").data("sort-dir", null).removeClass("sorting-desc sorting-asc");
-          $this.data("sort-dir", sort_dir).addClass("sorting-"+sort_dir);
-
-          // Trigger `aftertablesort` event. Similar to `beforetablesort`
-          $table.trigger("aftertablesort", {column: th_index, direction: sort_dir});
-          // More reliable method of forcing a redraw
-          $table.css("display");
-        }, 10);
+        $.fn.stupidtable.sortByColumn.call($table, th_index, sort_dir);
       });
     });
   };
+  
+  $.fn.stupidtable.sortByColumn = function (th_index, sort_dir) {
+    var $table = $(this);
 
+    var $this = $.fn.stupidtable.getColumnByIndex.call($table, th_index);
+
+    // Prevent sorting on invalid column indexes
+    if ($this === null) {
+      return;
+    }
+    var dir = $.fn.stupidtable.dir;
+
+    // Restore the sort functions
+    var sortFns = $table.data("sortFns");
+
+    // Choose appropriate sorting function.
+    var type = $this.data("sort") || null;
+
+    // Prevent sorting if no type defined
+    if (type === null) {
+      return;
+    }
+
+    // Trigger 'beforetablesort' event that calling scripts can hook into;
+    // pass parameters for sorted column index and sorting direction
+    $table.trigger("beforetablesort", {column: th_index, direction: sort_dir});
+
+    // More reliable method of forcing a redraw
+    $table.css("display");
+
+    // Run sorting asynchronously on a timeout to force browser redraw after
+    // 'beforetablesort' callback. Also avoids locking up the browser too much.
+    setTimeout(function() {
+      // Gather the elements for this column
+      var column = [];
+      var sortMethod = sortFns[type];
+      var trs = $table.children("tbody").children("tr");
+
+      // Extract the data for the column that needs to be sorted and pair it up
+      // with the TR itself into a tuple
+      trs.each(function(index,tr) {
+        var $e = $(tr).children().eq(th_index);
+        var sort_val = $e.data("sort-value");
+        var order_by = typeof(sort_val) !== "undefined" ? sort_val : $e.text();
+        column.push([order_by, tr]);
+      });
+
+      // Sort by the data-order-by value
+      column.sort(function(a, b) { return sortMethod(a[0], b[0]); });
+      if (sort_dir != dir.ASC)
+        column.reverse();
+
+      // Replace the content of tbody with the sorted rows. Strangely (and
+      // conveniently!) enough, .append accomplishes this for us.
+      trs = $.map(column, function(kv) { return kv[1]; });
+      $table.children("tbody").append(trs);
+
+      // Reset siblings
+      $table.find("th").data("sort-dir", null).removeClass("sorting-desc sorting-asc");
+      $this.data("sort-dir", sort_dir).addClass("sorting-"+sort_dir);
+
+      // Trigger 'aftertablesort' event. Similar to `beforetablesort`
+      $table.trigger("aftertablesort", {column: th_index, direction: sort_dir});
+
+      // More reliable method of forcing a redraw
+      $table.css("display");
+    }, 10);
+  };
+  
+  $.fn.stupidtable.getColumnByIndex = function (th_index) {
+    var $table = $(this);
+    var $this = null;
+    var cur_th_index = 0;
+    var found = false;
+
+    // Account for colspans
+    $table.find("thead th").each(function() {
+      if (cur_th_index >= th_index && !found) {
+        found = true;
+        $this = $(this);
+      }
+      var cols = $(this).attr("colspan") || 1;
+      cur_th_index += parseInt(cols,10);
+    });
+
+    return $this;
+  };
+  
+  $.fn.stupidtable.remove = function () {
+    var $table = $(this);
+    $table.data("sortFns", null);
+    $table.off("click.stupidtable", "thead th");
+  };
+  
   // Enum containing sorting directions
   $.fn.stupidtable.dir = {ASC: "asc", DESC: "desc"};
 
